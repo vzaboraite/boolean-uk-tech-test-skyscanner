a8f25ebfc4005f06cf921f78b1161fe82626f808
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-01-07 17:57:57 +0000

    Fetch flights data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BpkText from 'bpk-component-text';
 
 import Header from '../Header';
@@ -7,14 +7,32 @@ import STYLES from './App.scss';
 
 const getClassName = (className) => STYLES[className] || 'UNKNOWN';
 
-const App = () => (
-  <div className={getClassName('App')}>
-    <Header />
-    <main className={getClassName('App__main')}>
-      <BpkText tagName="p">Over to you...</BpkText>
-      {/* TODO: Add a component to display results here */}
-    </main>
-  </div>
-);
+const App = () => {
+  console.log('Inside App.jsx');
+  const [itineraries, setItineraries] = useState([]);
+  const [legs, setLegs] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/flights.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setItineraries(data.itineraries);
+        setLegs(data.legs);
+      });
+  }, []);
+
+  console.log({ itineraries });
+  console.log({ legs });
+
+  return (
+    <div className={getClassName('App')}>
+      <Header />
+      <main className={getClassName('App__main')}>
+        <BpkText tagName="p">Over to you...</BpkText>
+        {/* TODO: Add a component to display results here */}
+      </main>
+    </div>
+  );
+};
 
 export default App;
