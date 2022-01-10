7f7f79d96a838b2ec0dda33a69eeacd17d839d5a
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-01-10 20:28:55 +0000

    Refactor FlightsListItem.jsx, apply styles

--- src/components/FlightsList/components/FlightsListItem/FlightsListItem.jsx
+++ src/components/FlightsList/components/FlightsListItem/FlightsListItem.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import convertFlightDuration from '../../../../utils/flightDurationConverter';
 import convertFlightTime from '../../../../utils/flightTimeConverter';
 
+import BpkText from 'bpk-component-text';
+
+import LongArrowRightIcon from 'bpk-component-icon/sm/long-arrow-right';
+
 import STYLES from './FlightsListItem.scss';
 
 const getClassName = (className) => STYLES[className] || 'UNKNOWN';
@@ -11,7 +15,7 @@ const FlightsListItem = ({ flight }) => {
 
   return (
     <li className={getClassName('FlightsListItem')}>
-      <div className={getClassName('FlightsListItem__legs__grid')}>
+      <div className={getClassName('FlightsListItem__legs')}>
         {legs.map((leg) => {
           const {
             id: legId,
@@ -31,72 +35,115 @@ const FlightsListItem = ({ flight }) => {
           const convertedFlightDuration = convertFlightDuration(duration);
 
           return (
-            <React.Fragment key={legId}>
+            <div
+              className={getClassName('FlightsListItem__legs__leg')}
+              key={legId}
+            >
+              <img
+                src={`https://logos.skyscnr.com/images/airlines/favicon/${airline_id}.png`}
+                alt={`${airline_name}`}
+                className={getClassName(
+                  'FlightsListItem__legs__leg__airline-image'
+                )}
+              />
               <div
-                className={getClassName('FlightsListItem__legs__trip__grid')}
+                className={getClassName(
+                  'FlightsListItem__legs__leg__departure'
+                )}
               >
-                <img
-                  src={`https://logos.skyscnr.com/images/airlines/favicon/${airline_id}.png`}
-                  alt={`${airline_name}`}
+                <BpkText
+                  tagName="p"
                   className={getClassName(
-                    'FlightsListItem__legs__airline-image'
+                    'FlightsListItem__legs__leg__departure__time'
                   )}
-                />
-                <div
-                  className={getClassName('FlightsListItem__legs__departure')}
                 >
-                  <div
-                    className={getClassName(
-                      'FlightsListItem__legs__departure__time'
-                    )}
-                  >
-                    {convertedDepartureTime}
-                  </div>
-                  <div
-                    className={getClassName(
-                      'FlightsListItem__legs__departure__airport'
-                    )}
-                  >
-                    {departure_airport}
-                  </div>
-                </div>
-                <div>-&gt;</div>
-                <div className={getClassName('FlightsListItem__legs__arrival')}>
-                  <div
-                    className={getClassName(
-                      'FlightsListItem__legs__arrival__time'
-                    )}
-                  >
-                    {convertedArrivalTime}
-                  </div>
-                  <div
-                    className={getClassName(
-                      'FlightsListItem__legs__arrival__airport'
-                    )}
-                  >
-                    {arrival_airport}
-                  </div>
-                </div>
+                  {convertedDepartureTime}
+                </BpkText>
+                <BpkText
+                  tagName="p"
+                  className={getClassName(
+                    'FlightsListItem__legs__leg__departure__airport'
+                  )}
+                >
+                  {departure_airport}
+                </BpkText>
+              </div>
+              <BpkText textStyle="base">
+                <LongArrowRightIcon
+                  className={getClassName('FlightsListItem__legs__leg__arrow')}
+                />
+              </BpkText>
+              <div
+                className={getClassName('FlightsListItem__legs__leg__arrival')}
+              >
+                <BpkText
+                  tagName="p"
+                  className={getClassName(
+                    'FlightsListItem__legs__leg__arrival__time'
+                  )}
+                >
+                  {convertedArrivalTime}
+                </BpkText>
+                <BpkText
+                  tagName="p"
+                  className={getClassName(
+                    'FlightsListItem__legs__leg__arrival__airport'
+                  )}
+                >
+                  {arrival_airport}
+                </BpkText>
               </div>
               <div
                 className={getClassName(
-                  'FlightsListItem__legs__duration-stops'
+                  'FlightsListItem__legs__leg__duration-stops'
                 )}
               >
-                <div>{convertedFlightDuration}</div>
-                <div>{stops === 0 ? `Direct` : `${stops} Stop`}</div>
+                <BpkText
+                  className={getClassName(
+                    'FlightsListItem__legs__leg__duration-stops__duration'
+                  )}
+                  tagName="p"
+                >
+                  {convertedFlightDuration}
+                </BpkText>
+                <BpkText
+                  className={getClassName(
+                    `FlightsListItem__legs__leg__duration-stops__stops__${
+                      stops === 0 ? 'primary' : 'red'
+                    }`
+                  )}
+                  tagName="p"
+                >
+                  {stops === 0 ? `Direct` : `${stops} Stop`}
+                </BpkText>
               </div>
-            </React.Fragment>
+            </div>
           );
         })}
       </div>
 
-      <div className={getClassName('FlightsListItem__agent_select__grid')}>
-        <div className={getClassName('FlightsListItem__agent_price')}>
-          <div>{price}</div>
-          <div>{agent.toLowerCase()}</div>
+      <div className={getClassName('FlightsListItem__select')}>
+        <div className={getClassName('FlightsListItem__select__agent-price')}>
+          <BpkText
+            className={getClassName(
+              'FlightsListItem__select__agent-price__price'
+            )}
+            tagName="p"
+          >
+            {price}
+          </BpkText>
+          <BpkText
+            className={getClassName(
+              'FlightsListItem__select__agent-price__agent'
+            )}
+            tagName="p"
+          >
+            {agent.toLowerCase()}
+          </BpkText>
         </div>
-        <button>Select</button>
+        <button className={getClassName('FlightsListItem__select__button')}>
+          Select
+        </button>
       </div>
     </li>
   );
